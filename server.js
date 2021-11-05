6db0f72259cc97c1c87b22d6091888f330d1bf9f
Matthew Pagan <mastapegs01@gmail.com>
2021-11-05 14:11:31 -0400

    feat: create route for pokemon

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const fetch = require('cross-fetch');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+async function getPokemon(id) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data = await response.json();
+  return data;
+}
+
 app.get(["/", "/:name"], (req, res) => {
-  let greeting = "<h1>Hello From Node on Fly!</h1>";
-  let name = req.params["name"];
+  const greeting = "<h1>Hello From Node on Fly!</h1>";
+  const name = req.params["name"];
+
   if (name) {
     res.send(greeting + "</br>and hello to " + name);
   } else {
@@ -12,5 +20,10 @@ app.get(["/", "/:name"], (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+app.get("/pokemon/:id", async (req, res) => {
+  const id = req.params["id"];
+  const pokemon = await getPokemon(id);
+  res.send(pokemon.name);
+});
 
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
